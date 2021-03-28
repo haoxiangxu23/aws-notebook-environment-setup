a7fd7c1567e8c78a70ba55677b226ab038639783
Haoxiang Xu <80202441+haoxiangxu23@users.noreply.github.com>
2021-03-28 10:46:27 +0800

    Update jupyter_notebook_config.py

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -9,5 +9,6 @@ c.NotebookApp.allow_origin = '*' # put your public IP Address here
 c.NotebookApp.ip = '*'
 c.NotebookApp.allow_remote_access = True
 c.NotebookApp.open_browser = False
+# ipython -c "from notebook.auth import passwd; passwd()" // Using this command to change password
 # c.NotebookApp.password = u'argon2:$argon2id$v=19$m=10240,t=10,p=8$/DJ3H3hTXpz6JW3P0rCmhw$wNOqs6zfdHwLMwqNQRIjbA'
 c.NotebookApp.port = 8888
